60ba92f6bbfd42cfe30dea1b518d65f5c126497d
SimulatedGREG <simulatedgreg@gmail.com>
2016-10-17 00:20:58 -0500

    bind asap, no need to wait for onReady cb

--- src/vue-horizon.js
+++ src/vue-horizon.js
@@ -2,20 +2,28 @@ var Horizon = require('@horizon/client')
 
 module.exports = {
   install: function (Vue, opts) {
+    // Instantiate Horizon
     var horizon = new Horizon(opts || {})
 
+    // Bind instance to Vue
+    Vue.prototype.$horizon = horizon
+
+    /**
+     * Attach optional Horizon models to Vue
+     * [this.$$model]
+     */
+    if (opts.models) {
+      opts.models.forEach(function (model) {
+        Vue.prototype['$$' + model] = horizon(model)
+      })
+    }
+
     horizon.onReady(() => {
-      Vue.prototype.$horizon = horizon
       if (process.env.NODE_ENV !== 'production') {
         console.log('[vue-horizon] Ready. Connected to ' + opts.host)
       }
-
-      if (opts.models) {
-        for (var i = 0; i < opts.models.length; i++) {
-          Vue.prototype['$$' + opts.models[i]] = horizon(opts.models[i])
-        }
-      }
     })
+
     horizon.connect()
   }
 }
